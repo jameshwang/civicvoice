e5cac91353a72b7448f7f7eef33bf47e3885e6cb
James Hwang <james.hwang.usc@gmail.com>
2012-09-08 15:48:17 -0700

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Epetition::Application.config.secret_token = 'be9adae59ac5059a4d38f1b816faf5181a73af1f2d05f0aa6521e2191eaa4190f5cd7348e0d5c949b0b6ca9b1926b3b8da02cf96c12bedbdca2c1b86a1feccd2'
